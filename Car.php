2d503866003feb76344f8d8e5c784b19f52ecab8
Chris Lytsell & Mike Rapp <student@epicodus.com>
2015-08-03 11:19:24 -0700

    add constructor.

--- Car.php
+++ Car.php
@@ -4,27 +4,33 @@ class Car
     public $make_model;
     public $price;
     public $miles;
-}
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
+ function __construct($make_model, $price, $miles)
+ {
+   $this->make_model = $make_model;
+   $this->price = $price;
+   $this->miles = $miles;
+ } 
+ }
+$porsche = new Car("2014 Porsche 911", 114991, 7864);
+//$porsche->make_model = "2014 Porsche 911";
+//$porsche->price = 114991;
+//$porsche->miles = 7864;
 
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
+$ford = new Car("2001 Ford", 55995, 14241);
+/*$ford->make_model = "2011 Ford F450";
 $ford->price = 55995;
-$ford->miles = 14241;
+$ford->miles = 14241;*/
 
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+/*$lexus->make_model = "2013 Lexus RX 350";
 $lexus->price = 44700;
-$lexus->miles = 20000;
+$lexus->miles = 20000;*/
 
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+/*$mercedes->make_model = "Mercedes Benz CLS550";
 $mercedes->price = 39900;
-$mercedes->miles = 37979;
+$mercedes->miles = 37979;*/
 
 $cars = array($porsche, $ford, $lexus,$mercedes);
 
